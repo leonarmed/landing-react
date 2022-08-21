495a46d3f8476dba9d3b081ef67c17f519b9d8f9
Leonardo Medina <leonarmed@gmail.com>
2022-08-21 01:55:14 +0000

    initial upload

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,96 @@
 import React from "react";
+import CustomCard from "./CustomCard/index.jsx";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import CustomNavbar from './CustomNavbar/index.jsx';
+import Jumbotron from "./Jumbotron/index.jsx";
 
+const cards = [
+	{
+		imgContainer:{
+			url:"https://acnews.blob.core.windows.net/imgnews/medium/NAZ_c8d202cc16cf4b079ffd88860a0c0866.jpg",
+			alt:"Primer viaje"
+		},
+		title: "Viaje 1",
+		description: "Descripción viaje 1",
+		buttonContainer:{
+			url:"www.google.com",
+		}
+	},
+	{
+		imgContainer:{
+			url:"https://chevrolet.delasobera.com.py/images/products/s10/chevrolet-s10-cabina-doble-pick-up-exterior.webp",
+			alt:"Segundo viaje"
+		},
+		title: "Viaje 2",
+		description: "Descripción viaje 2",
+		buttonContainer:{
+			url:"www.google.com",
+		}
+	},
+	{
+		imgContainer:{
+			url:"https://paraguaysobreruedas.com/wp-content/uploads/2021/01/Capa-Nueva-Chevrolet-S10.jpg",
+			alt:"Tercer viaje"
+		},
+		title: "Viaje 3",
+		description: "Descripción viaje 3",
+		buttonContainer:{
+			url:"www.google.com",
+		}
+	},
+	{
+		imgContainer:{
+			url:"https://www.megautos.com/wp-content/uploads/2021/10/Chevrolet-S10-Z71-delantera.jpg",
+			alt:"Cuarto viaje"
+		},
+		title: "Viaje 4",
+		description: "Descripción viaje 4",
+		buttonContainer:{
+			url:"www.google.com",
+		}
+	},
+	{
+		imgContainer:{
+			url:"https://www.clarin.com/img/2022/05/19/chevrolet-s10-z71-la-nueva___p8nj-P-pn_2000x1500__1.jpg",
+			alt:"Quinto viaje"
+		},
+		title: "Viaje 5",
+		description: "Descripción viaje 5",
+		buttonContainer:{
+			url:"www.google.com",
+		}
+	},
+	{
+		imgContainer:{
+			url:"https://www.chevrolet.com.ar/content/dam/chevrolet/mercosur/argentina/espanol/index/pickups-and-trucks/2022-s10-z71/mov/01-images/acessorios-desk.jpg?imwidth=960",
+			alt:"Sexto viaje"
+		},
+		title: "Viaje 6",
+		description: "Descripción viaje 6",
+		buttonContainer:{
+			url:"www.google.com",
+		}
+	}
+]
 //create your first component
 const Home = () => {
+	var currentTime = new Date();
+	var year = currentTime.getFullYear()
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<CustomNavbar />
+			<div className="container-md p-2">
+				<Jumbotron />
+				<div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 m-auto">
+					{cards.map(card => {
+						return <CustomCard imgContainer={card.imgContainer} title={card.title} description={card.description} buttonContainer={card.buttonContainer}/>
+					})}
+				</div>
+			</div>
+			<div className="footer w-100 bg-dark text-white d-flex justify-content-center align-items-center fs-6 fw-light">
+				Copyright Reserved - Travels PY {year}
+			</div>
 		</div>
 	);
 };
